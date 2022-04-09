5d021f2881fafe0b1552efe3436885350e33ccbe
yvonneechoo <yvonnee.choox@gmail.com>
2022-04-10 00:30:14 +0800

    finalised the code

---
+++ js/front.js
@@ -0,0 +1,10 @@
+// side bar for small screen
+
+var navLinks = document.getElementById("navLinks");
+
+function showMenu(){
+    navLinks.style.right = "0";
+}
+function hideMenu(){
+    navLinks.style.right = "-200px";
+}
